725764ef2fd23c1e708e39bc5f94342ea2c38b29
Patrick Trabocchi <patricktrabocchi@openforce.it>
2025-11-04 11:29:30 +0100

    [IMP] add check to avoid delete testing / staging branch

--- src/orco_bot/tasks/delete_branch.py
+++ src/orco_bot/tasks/delete_branch.py
@@ -1,6 +1,8 @@
 # Copyright (c) ACSONE SA/NV 2018
 # Distributed under the MIT License (http://opensource.org/licenses/MIT).
 
+import re
+
 from .. import github
 from ..config import switchable
 from ..github import gh_call
@@ -12,10 +14,14 @@ _logger = getLogger(__name__)
 @task()
 @switchable()
 def delete_branch(org, repo, branch, dry_run=False):
+    test_branch_regex = '^[0-9][0-9]?\.[0-9]-test(ing)?$'
     with github.repository(org, repo) as gh_repo:
         gh_branch = gh_call(gh_repo.ref, f"heads/{branch}")
+        regex = re.compile(test_branch_regex)
         if dry_run:
             _logger.info(f"DRY-RUN delete branch {branch} in {org}/{repo}")
+        elif regex.match(branch):
+            _logger.info(f"{branch} is a test branch. Not deleting it")
         else:
             _logger.info(f"deleting branch {branch} in {org}/{repo}")
             gh_call(gh_branch.delete)
